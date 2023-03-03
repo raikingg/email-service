7d8f2e641afa5674440775d70af35f79eae53132
Shivanand Rai <raikingg@gmail.com>
2023-03-03 11:29:28 +0530

    First Commit

---
+++ src/main/java/shivan/emailservice/EmailServiceApp/EmailServiceAppApplication.java
@@ -0,0 +1,13 @@
+package shivan.emailservice.EmailServiceApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailServiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailServiceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shivan/emailservice/EmailServiceApp/EmailServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package shivan.emailservice.EmailServiceApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
